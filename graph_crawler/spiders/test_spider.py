09e6f7b42a2533c526e692af405860296d31e6c1
samuelcwilliams <samcwilliams67@gmail.com>
2022-12-13 17:03:38 -0600

    implemented string formatting to yield correct output for availability

--- graph_crawler/spiders/test_spider.py
+++ graph_crawler/spiders/test_spider.py
@@ -16,5 +16,5 @@ class DemoSpider(CrawlSpider):
         yield {
             "title": response.css(".product_main h1::text").get(),
             "price": response.css(".price_color::text").get(),
-            "availability": response.css(".availability::text")[1].get(),
+            "availability": response.css(".availability::text")[1].get().replace('\n', '').replace(' ', ''),
         }
